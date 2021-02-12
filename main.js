a19ba2b9a173ec193a2322df66be7f354122d217
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 14:30:19 +0100

    Initial commit

--- main.js
+++ main.js
@@ -11,86 +11,92 @@ const footer = document.getElementById("footer-main");
 const loginform = document.getElementById("login");
 loginform.classList.remove("hidden");
 
-function checkKey() {
+var media = window.matchMedia("(max-width: 375px)")
 
+function checkKey() {
+  const inputKey = inputKeyField.value;
   // Retrieve text
-  fetch("main.js-key.txt")
+  fetch("0.txt")
     .then(response => response.text())
     .then(textData => {
-      console.log(textData);
+      console.log("Text: " + textData);
+      if (textData === inputKey) {
+        keyCorrect()
+      } else if (document.getElementById("key").value == "") {
+        keyNone()
+      } else {
+        keyError()
+      }
     })
     .catch(error => {
       console.error('Error:', error);
     });
+}
 
-  const key = "jovan";
-  const inputKey = inputKeyField.value;
-  var media = window.matchMedia("(max-width: 375px)")
+function keyCorrect() {
 
   // Input
-  if (key === inputKey) {
-    if (media.matches) {
-      Swal.fire({
-        text: "Richtiger Key angegeben!",
-        icon: "success",
-        showConfirmButton: false,
-        timer: 1500
-      })
-    } else {
-      Swal.fire({
-        title: "Richtiger Key angegeben!",
-        icon: "success",
-        showConfirmButton: false,
-        timer: 1500
-      })
-    }
-    downloadButton.classList.remove("hidden");
-    downloadDiv.classList.add("download-div");
-    loginform.classList.add("hidden");
-
-  } else if (document.getElementById("key").value == "") {
-    if (media.matches) {
-      Swal.fire({
-        text: "Kein Key angegeben!",
-        icon: "warning",
-        showConfirmButton: false,
-        timer: 1500
-      })
-    } else {
-      Swal.fire({
-        title: "Kein Key angegeben!",
-        icon: "warning",
-        showConfirmButton: false,
-        timer: 1500
-      })
-    }
+  if (media.matches) {
+    Swal.fire({
+      text: "Richtiger Key angegeben!",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500
+    })
   } else {
-    if (media.matches) {
-      Swal.fire({
-        text: "Falscher Key!",
-        icon: "error",
-        confirmButtonText: "OK"
-      }).then(() => {
-        window.history.back()
-      });
-    } else {
-      Swal.fire({
-        title: "Falscher Key!",
-        icon: "error",
-        confirmButtonText: "OK"
-      }).then(() => {
-        window.history.back()
-      });
-    }
-    /*downloadButton.classList.add("hidden");
-    downloadDiv.classList.remove("download-div");
-    comingsoon.classList.remove("hidden");
-    loginform.classList.remove("hidden");*/
+    Swal.fire({
+      title: "Richtiger Key angegeben!",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+  downloadButton.classList.remove("hidden");
+  downloadDiv.classList.add("download-div");
+  loginform.classList.add("hidden");
+}
 
-
-
+function keyNone() {
+  if (media.matches) {
+    Swal.fire({
+      text: "Kein Key angegeben!",
+      icon: "warning",
+      showConfirmButton: false,
+      timer: 1500
+    })
+  } else {
+    Swal.fire({
+      title: "Kein Key angegeben!",
+      icon: "warning",
+      showConfirmButton: false,
+      timer: 1500
+    })
   }
 }
+
+function keyError() {
+  if (media.matches) {
+    Swal.fire({
+      text: "Falscher Key!",
+      icon: "error",
+      confirmButtonText: "OK"
+    }).then(() => {
+      window.history.back()
+    });
+  } else {
+    Swal.fire({
+      title: "Falscher Key!",
+      icon: "error",
+      confirmButtonText: "OK"
+    }).then(() => {
+      window.history.back()
+    });
+  }
+}
+/*downloadButton.classList.add("hidden");
+downloadDiv.classList.remove("download-div");
+comingsoon.classList.remove("hidden");
+loginform.classList.remove("hidden");*/
 
 // Download
 document.getElementById("download-button").addEventListener("click", download);
