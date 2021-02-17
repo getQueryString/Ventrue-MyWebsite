527ffaf80eb8e69259eeab3bbcc1944de4a51eb8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 13:59:59 +0100

    Initial commit

--- Beweise.php
+++ Beweise.php
@@ -3,135 +3,133 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <title>Beweise</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Beweise</title>
 </head>
 <noscript>
-<li><a href="https://support.google.com/adsense/answer/12654?hl=de" />JavaScript ist deaktiviert.</a></li>
-  <link href="jsactive.css" rel="stylesheet">
+    <li><a href="https://support.google.com/adsense/answer/12654?hl=de"/>JavaScript ist deaktiviert.</a></li>
+    <link href="jsactive.css" rel="stylesheet">
 </noscript>
 <body>
-   <!--
-   $ip = $_SERVER["REMOTE_ADDR"];
-   $mip = "x";
+<!--
+$ip = $_SERVER["REMOTE_ADDR"];
+$mip = "x";
 
-   if($ip == $mip):?>
-   <style>#login{display: none;}</style>
- endif; -->
-  <form id="login" method="POST" action="">
+if($ip == $mip):?>
+<style>#login{display: none;}</style>
+endif; -->
+<form id="login" method="POST" action="">
     <input type="text" id="key" placeholder="Key" name="key">
     <input type="submit" id="login-button" name="login" value="Login">
-  </form>
+</form>
 
-    <script>var media = window.matchMedia("(max-width: 375px)");</script>
-      <?php
-  if(x){
+<script>var media = window.matchMedia("(max-width: 375px)");</script>
+<?php
+if (x) {
     x
-    if(x){
-
-      x?>
-      <script>x</script>
-      <?php
-      if(x){
-
+    if (x) {
+x
+    <?php
+    if (x) {
         x
-      } else{
-          x
-        }
+    } else {
+        x
+    }
 
-    }else if($key == ""){
-      ?>
-          <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-          <script>
+    }else if (x){
+    ?>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <script>
             if (media.matches) {
                 Swal.fire({
-                  text: "Kein Key angegeben!",
-                  icon: "warning",
-                  showConfirmButton: false,
-                  allowEscapeKey: false,
-                  allowOutsideClick: false,
-                  timer: 1500
+                    text: "Kein Key angegeben!",
+                    icon: "warning",
+                    showConfirmButton: false,
+                    allowEscapeKey: false,
+                    allowOutsideClick: false,
+                    timer: 1500
                 })
-              } else {
+            } else {
                 Swal.fire({
-                  title: "Kein Key angegeben!",
-                  icon: "warning",
-                  showConfirmButton: false,
-                  allowEscapeKey: false,
-                  allowOutsideClick: false,
-                  timer: 1500
+                    title: "Kein Key angegeben!",
+                    icon: "warning",
+                    showConfirmButton: false,
+                    allowEscapeKey: false,
+                    allowOutsideClick: false,
+                    timer: 1500
                 })
-              }</script>
-      <?php
+            }</script>
+    <?php
     }else{
-      ?>
-          <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-          <script>
-          if (media.matches) {
-            Swal.fire({
-              text: "Falscher Key!",
-              icon: "error",
-              allowEscapeKey: false,
-              allowOutsideClick: false,
-              confirmButtonText: "OK"
-            }).then(() => {
-              window.history.back()
-            });
-          } else {
-            Swal.fire({
-              title: "Falscher Key!",
-              icon: "error",
-              allowEscapeKey: false,
-              allowOutsideClick: false,
-              confirmButtonText: "OK"
-            })
-          }</script>
-      <?php
+    ?>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <script>
+            if (media.matches) {
+                Swal.fire({
+                    text: "Falscher Key!",
+                    icon: "error",
+                    allowEscapeKey: false,
+                    allowOutsideClick: false,
+                    confirmButtonText: "OK"
+                }).then(() => {
+                    window.history.back()
+                });
+            } else {
+                Swal.fire({
+                    title: "Falscher Key!",
+                    icon: "error",
+                    allowEscapeKey: false,
+                    allowOutsideClick: false,
+                    confirmButtonText: "OK"
+                })
+            }</script>
+        <?php
     }
-  }
+}
 ?>
-  <header>
+<header>
     <h1>Beweise</h1>
     <nav>
-      <ul>
-        <li><a href="index.php">» Start •</a></li>
-      </ul>
+        <ul>
+            <li><a href="index.php">» Start •</a></li>
+        </ul>
     </nav>
-  </header>
-  <hr / id="mlg-hr-top">
-  <!-- Download -->
-  <!--<div id="download-div" class="download-div hidden">
-  <form method="POST" action="download-process.php">
-    <button name="download-button">DOWNLOAD</button>
-  </form>-->
-  </div>
+</header>
+<hr
+/ id="mlg-hr-top">
+<!-- Download -->
+<!--<div id="download-div" class="download-div hidden">
+<form method="POST" action="download-process.php">
+  <button name="download-button">DOWNLOAD</button>
+</form>-->
+</div>
 </body>
-  <!--<script type="text/javascript" src="main.js"></script>-->
+<!--<script type="text/javascript" src="main.js"></script>-->
 <footer class="footer-main" id="footer-main">
-  <div id="footer1">
-    <ul>
-      <li><a href="https://github.com/getQueryString" />GitHub</a></li>
-    </ul>
-  </div>
-  <div id="footer2">
-    <ul>
-      <li><a href="https://www.youtube.com/c/MrDudeCraftHD" />YouTube</a></li>
-    </ul>
-  </div>
-  <div id="footer3">
-    <ul>
-      <li><a href="https://ventrue.eu/Forum/" />Forum</a></li>
-    </ul>
-  </div>
-  <div id="footer4">
-    <ul>
-      <li><a href="ts3server://Ventrue.EU" />TeamSpeak</a></li>
-    </ul>
-  </div>
-  <div id="footer-p">
-    <p>| » Copyright © 2021 getQueryString - All rights reserved. |</p>
-  </div>
+    <div id="footer1">
+        <ul>
+            <li><a href="https://github.com/getQueryString"/>GitHub</a></li>
+        </ul>
+    </div>
+    <div id="footer2">
+        <ul>
+            <li><a href="https://www.youtube.com/c/MrDudeCraftHD"/>YouTube</a></li>
+        </ul>
+    </div>
+    <div id="footer3">
+        <ul>
+            <li><a href="https://ventrue.eu/Forum/"/>Forum</a></li>
+        </ul>
+    </div>
+    <div id="footer4">
+        <ul>
+            <li><a href="ts3server://Ventrue.EU"/>TeamSpeak</a></li>
+        </ul>
+    </div>
+    <div id="footer-p">
+        <p>| » Copyright © 2021 getQueryString - All rights reserved. |</p>
+    </div>
 </footer>
 </html>
--- index.php
+++ index.php
@@ -4,49 +4,49 @@
 <html lang="de">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <title>Userinterface</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Userinterface</title>
 </head>
 
 <body>
-  <header>
+<header>
     <h1>Userinterface</h1>
     <nav>
-      <ul>
-        <li><a href="Beweise.php">» Beweise •</a></li>
-        <li><a href="Spielmodies.php">» Spielmodies •</a></li>
-      </ul>
+        <ul>
+            <li><a href="Beweise.php">» Beweise •</a></li>
+            <li><a href="Spielmodies.php">» Spielmodies •</a></li>
+        </ul>
     </nav>
-  </header>
-  <hr / id="mlg-hr-top">
+</header>
+<hr / id="mlg-hr-top">
 
 </body>
 <footer class="footer-main">
-  <div id="footer1">
-    <ul>
-      <li><a href="https://github.com/getQueryString" />GitHub</a></li>
-    </ul>
-  </div>
-  <div id="footer2">
-    <ul>
-      <li><a href="https://www.youtube.com/c/MrDudeCraftHD" />YouTube</a></li>
-    </ul>
-  </div>
-  <div id="footer3">
-    <ul>
-      <li><a href="https://ventrue.eu/Forum/" />Forum</a></li>
-    </ul>
-  </div>
-  <div id="footer4">
-    <ul>
-      <li><a href="ts3server://Ventrue.EU" />TeamSpeak</a></li>
-    </ul>
-  </div>
-  <div id="footer-p">
-    <p>| » Copyright © 2021 getQueryString - All rights reserved. |</p>
-  </div>
+    <div id="footer1">
+        <ul>
+            <li><a href="https://github.com/getQueryString"/>GitHub</a></li>
+        </ul>
+    </div>
+    <div id="footer2">
+        <ul>
+            <li><a href="https://www.youtube.com/c/MrDudeCraftHD"/>YouTube</a></li>
+        </ul>
+    </div>
+    <div id="footer3">
+        <ul>
+            <li><a href="https://ventrue.eu/Forum/"/>Forum</a></li>
+        </ul>
+    </div>
+    <div id="footer4">
+        <ul>
+            <li><a href="ts3server://Ventrue.EU"/>TeamSpeak</a></li>
+        </ul>
+    </div>
+    <div id="footer-p">
+        <p>| » Copyright © 2021 getQueryString - All rights reserved. |</p>
+    </div>
 </footer>
 
 </html>
