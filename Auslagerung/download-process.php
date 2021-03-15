cef904fd65db1ab84e9bdacfc95d6da841280a1b
BuildTools <unconfigured@null.spigotmc.org>
2021-03-15 03:12:58 +0100

    Initial commit

---
+++ Auslagerung/download-process.php
@@ -0,0 +1,24 @@
+<?php
+$filename = "x.zip";
+$filepath = "download/x.zip"; ?>
+<script>const filename = "x.zip";
+    const file_error = "Error: File " + filename + " does not found.";</script>
+<?php
+if (isset($_POST["download-button"])) {
+    if (file_exists($filepath)) {
+        // Define headers
+
+        header("Cache-Control: no-cache, no-store, must-revalidate");
+        header("Expires: 0");
+        header("Content-type: application/zip");
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
+        header("Content-Transfer-Encoding: binary");
+        header("Content-Length: " . filesize($filepath));
+        ob_end_flush();
+        readfile($filepath);
+    } else {
+        echo "<script>console.log(file_error)</script>";
+        echo "Error: File $filename does not found.";
+    }
+}
+?>
